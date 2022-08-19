f053d871e27769414c18df11b7e27beb8f8fc473
Kamal Bramwell <kamal@elixxir.io>
2022-08-18 20:04:21 -0400

    Version up 2.92/629

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
 
     defaultConfig {
         applicationId = "io.xxlabs.messenger"
-        versionCode = 628
-        versionName = "2.91"
+        versionCode = 629
+        versionName = "2.92"
         minSdk = 26
         targetSdk = 31
         testInstrumentationRunner = "io.xxlabs.messenger.CustomTestRunner"
