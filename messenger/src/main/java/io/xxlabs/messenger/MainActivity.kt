990fb94416387ad1feff86a50db0b7a18770ae37
Kamal Bramwell <kamal@elixxir.io>
2022-09-02 11:33:21 -0400

    Created MainActivity

---
+++ messenger/src/main/java/io/xxlabs/messenger/MainActivity.kt
@@ -0,0 +1,14 @@
+package io.xxlabs.messenger
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val INTENT_NOTIFICATION_CLICK = "nav_bundle"
+        const val INTENT_PRIVATE_CHAT = "private_message"
+        const val INTENT_GROUP_CHAT = "group_message"
+        const val INTENT_REQUEST = "request"
+        const val INTENT_INVITATION = "invitation"
+    }
+}
