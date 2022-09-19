dcd6b266d1f7634cba96aa7ea71934f83bbd0b27
Kamal Bramwell <kamal@elixxir.io>
2022-09-19 17:49:18 -0400

    Added CipherPreferences dependency to XxmKeystore

--- messenger/src/main/java/io/xxlabs/messenger/keystore/XxmKeystore.kt
+++ messenger/src/main/java/io/xxlabs/messenger/keystore/XxmKeystore.kt
@@ -7,20 +7,24 @@ import io.xxlabs.messenger.util.toBase64String
 import timber.log.Timber
 import java.security.*
 import java.security.spec.MGF1ParameterSpec
-import javax.crypto.BadPaddingException
 import javax.crypto.Cipher
-import javax.crypto.IllegalBlockSizeException
-import javax.crypto.NoSuchPaddingException
 import javax.crypto.spec.OAEPParameterSpec
 import javax.crypto.spec.PSource
 
-class XxmKeystore : KeyStoreManager {
-
-    private val preferences: CipherPreferences by lazy {
-        CipherPrefs()
-    }
+class XxmKeystore(private val preferences: CipherPreferences) : KeyStoreManager {
 
     override suspend fun generatePassword(): Result<Unit> {
+        deletePreviousKeys()
+
+        return try {
+            generateSecret()
+            Result.success(Unit)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    private fun generateSecret() {
         val bytesNumber: Long = 64
         var initialT = System.currentTimeMillis()
         Timber.v("[BASE KEYSTORE] Generating a password...")
@@ -40,16 +44,18 @@ class XxmKeystore : KeyStoreManager {
         initialT = System.currentTimeMillis()
         rsaEncryptPwd(secret)
         Timber.v("[BASE KEYSTORE] total encryption time: ${System.currentTimeMillis() - initialT}ms")
-        onFinished.invoke()
+
+
     }
 
-    @Throws(
-        NoSuchAlgorithmException::class,
-        NoSuchPaddingException::class,
-        InvalidKeyException::class,
-        IllegalBlockSizeException::class,
-        BadPaddingException::class
-    )
+    private fun deletePreviousKeys() {
+        val keystore = getKeystore()
+        if (keystore.containsAlias(KEY_ALIAS)) {
+            Timber.v("[BASE KEYSTORE] Deleting key alias")
+            keystore.deleteEntry(KEY_ALIAS)
+        }
+    }
+
     private fun rsaEncryptPwd(pwd: ByteArray): ByteArray {
         Timber.d("[BASE KEYSTORE] Bytecount: ${pwd.size}")
         Timber.d("[BASE KEYSTORE] Before encryption: ${pwd.toBase64String()}")
@@ -63,27 +69,22 @@ class XxmKeystore : KeyStoreManager {
         return encryptedBytes
     }
 
-        @Throws(
-        NoSuchAlgorithmException::class,
-        NoSuchPaddingException::class,
-        InvalidKeyException::class,
-        IllegalBlockSizeException::class,
-        BadPaddingException::class
-    )
     override fun rsaDecryptPwd(): Result<ByteArray> {
         return try {
-            val encryptedBytes = preferences.userSecret.fromBase64toByteArray()
-            val key = getPrivateKey()
-            val cipher1 = Cipher.getInstance(KEYSTORE_ALGORITHM)
-            println("[BASE KEYSTORE] Initializing Decrypt")
-            cipher1.init(Cipher.DECRYPT_MODE, key, cipherMode)
-            val decryptedBytes = cipher1.doFinal(encryptedBytes)
-            println("[BASE KEYSTORE] Decrypted: ${decryptedBytes.toBase64String()}")
-
-            Result.success(decryptedBytes)
+            Result.success(decryptSecret())
         } catch (e: Exception) {
             Result.failure(e)
         }
+    }
+
+    private fun decryptSecret(): ByteArray {
+        val encryptedBytes = preferences.userSecret.fromBase64toByteArray()
+        val key = getPrivateKey()
+        val cipher1 = Cipher.getInstance(KEYSTORE_ALGORITHM)
+        println("[BASE KEYSTORE] Initializing Decrypt")
+        cipher1.init(Cipher.DECRYPT_MODE, key, cipherMode)
+
+        return cipher1.doFinal(encryptedBytes)
     }
 
     private fun getPrivateKey(): PrivateKey? {
