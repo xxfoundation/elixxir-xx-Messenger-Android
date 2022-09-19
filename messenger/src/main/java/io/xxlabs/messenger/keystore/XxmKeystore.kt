321016296c290e2f2e881fe2a002820886f9779b
Kamal Bramwell <kamal@elixxir.io>
2022-09-19 18:04:53 -0400

    Log writer and Bindings now injected into XxmKeystore

--- messenger/src/main/java/io/xxlabs/messenger/keystore/XxmKeystore.kt
+++ messenger/src/main/java/io/xxlabs/messenger/keystore/XxmKeystore.kt
@@ -1,71 +1,71 @@
 package io.xxlabs.messenger.keystore
 
 import android.security.keystore.KeyProperties
-import bindings.Bindings
+import io.elixxir.xxclient.bindings.Bindings
 import io.xxlabs.messenger.util.fromBase64toByteArray
 import io.xxlabs.messenger.util.toBase64String
-import timber.log.Timber
 import java.security.*
 import java.security.spec.MGF1ParameterSpec
 import javax.crypto.Cipher
 import javax.crypto.spec.OAEPParameterSpec
 import javax.crypto.spec.PSource
+import kotlin.system.measureTimeMillis
 
-class XxmKeystore(private val preferences: CipherPreferences) : KeyStoreManager {
+class XxmKeystore(
+    private val bindings: Bindings,
+    private val preferences: CipherPreferences,
+    private val log: (String) -> Unit
+) : KeyStoreManager {
 
     override suspend fun generatePassword(): Result<Unit> {
         deletePreviousKeys()
 
         return try {
-            generateSecret()
+            val duration = measureTimeMillis {
+                generateSecret()
+            }
+            log("Total encryption time: $duration ms")
             Result.success(Unit)
         } catch (e: Exception) {
             Result.failure(e)
         }
     }
 
-    private fun generateSecret() {
-        val bytesNumber: Long = 64
-        var initialT = System.currentTimeMillis()
-        Timber.v("[BASE KEYSTORE] Generating a password...")
-        Timber.d("[BASE KEYSTORE] Generating a password with $bytesNumber bytes")
+    private fun generateSecret(length: Long = DEFAULT_SECRET_LENGTH) {
+        log("Generating a password with $length bytes")
 
         var secret: ByteArray
         do {
-            secret = Bindings.generateSecret(bytesNumber)
-            Timber.d("[BASE KEYSTORE] Password (Bytearray): $secret")
-            Timber.d("[BASE KEYSTORE] Password (String64): ${secret.toBase64String()}")
-            Timber.v("[BASE KEYSTORE] total generation time: ${System.currentTimeMillis() - initialT}ms")
+            secret = bindings.generateSecret(length)
+            log("Password (Bytearray): $secret")
+            log("Password (String64): ${secret.toBase64String()}")
 
-            val isAllZeroes = byteArrayOf(bytesNumber.toByte()).contentEquals(secret)
-            Timber.d("[BASE KEYSTORE] IsAllZeroes: $isAllZeroes")
+            val isAllZeroes = byteArrayOf(length.toByte()).contentEquals(secret)
+            log("IsAllZeroes: $isAllZeroes")
         } while (isAllZeroes)
 
-        initialT = System.currentTimeMillis()
         rsaEncryptPwd(secret)
-        Timber.v("[BASE KEYSTORE] total encryption time: ${System.currentTimeMillis() - initialT}ms")
-
-
     }
 
     private fun deletePreviousKeys() {
         val keystore = getKeystore()
         if (keystore.containsAlias(KEY_ALIAS)) {
-            Timber.v("[BASE KEYSTORE] Deleting key alias")
+            log("Deleting key alias")
             keystore.deleteEntry(KEY_ALIAS)
         }
     }
 
     private fun rsaEncryptPwd(pwd: ByteArray): ByteArray {
-        Timber.d("[BASE KEYSTORE] Bytecount: ${pwd.size}")
-        Timber.d("[BASE KEYSTORE] Before encryption: ${pwd.toBase64String()}")
+        log("Byte count: ${pwd.size}")
+        log("Before encryption: ${pwd.toBase64String()}")
         val secretKey = getPublicKey()
 
         val cipher = Cipher.getInstance(KEYSTORE_ALGORITHM)
         cipher.init(Cipher.ENCRYPT_MODE, secretKey, cipherMode)
         val encryptedBytes = cipher.doFinal(pwd)
-        Timber.v("[BASE KEYSTORE] Encrypted: ${encryptedBytes.toBase64String()}")
+        log("Encrypted: ${encryptedBytes.toBase64String()}")
         preferences.userSecret = encryptedBytes.toBase64String()
+
         return encryptedBytes
     }
 
@@ -80,11 +80,11 @@ class XxmKeystore(private val preferences: CipherPreferences) : KeyStoreManager 
     private fun decryptSecret(): ByteArray {
         val encryptedBytes = preferences.userSecret.fromBase64toByteArray()
         val key = getPrivateKey()
-        val cipher1 = Cipher.getInstance(KEYSTORE_ALGORITHM)
-        println("[BASE KEYSTORE] Initializing Decrypt")
-        cipher1.init(Cipher.DECRYPT_MODE, key, cipherMode)
+        val cipher = Cipher.getInstance(KEYSTORE_ALGORITHM)
+        println("Initializing Decrypt")
+        cipher.init(Cipher.DECRYPT_MODE, key, cipherMode)
 
-        return cipher1.doFinal(encryptedBytes)
+        return cipher.doFinal(encryptedBytes)
     }
 
     private fun getPrivateKey(): PrivateKey? {
@@ -103,6 +103,7 @@ class XxmKeystore(private val preferences: CipherPreferences) : KeyStoreManager 
     }
 
     companion object {
+        private const val DEFAULT_SECRET_LENGTH = 64L
         private const val KEY_ALIAS = "xxmessengerpvk"
         private const val ANDROID_KEYSTORE = "AndroidKeyStore"
         private const val KEY_PURPOSE =
