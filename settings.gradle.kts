9bb1a93b895f5b340bd7e5235e39d49839298b76
Kamal Bramwell <kamal@elixxir.io>
2022-09-22 14:55:46 -0400

    Updated settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,24 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+        maven(url = "https://maven.appspector.com/artifactory/android-sdk")
+        maven(url = "https://oss.jfrog.org/libs-snapshot")
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "xx Messenger"
+
 include(":app")
 
 // Feature modules
@@ -5,7 +26,6 @@ include(":feature:registration")
 
 // Data modules
 include(":data:session")
-include(":data:proto")
 
 // Core modules
 include(":core:ui")
@@ -15,8 +35,11 @@ include(":core:preferences")
 include(":depconstraints")
 
 // Cmix/Client modules
-include(":xxclient:cmix")
-project(":xxclient:cmix").projectDir = File("elixxir-dapps-sdk-kotlin/cmix")
-
-include("xxclient")
+include(":xxclient")
 project(":xxclient").projectDir = File("elixxir-dapps-sdk-kotlin/xxclient")
+
+include(":cmix")
+project(":cmix").projectDir = File("elixxir-dapps-sdk-kotlin/cmix")
+
+// Protobuf
+include(":proto")
