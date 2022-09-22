2386cdc13d95262162fcd5a6a5c07cb0ce681e4f
Kamal Bramwell <kamal@elixxir.io>
2022-09-22 12:29:28 -0400

    Created :core:ui module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,11 +2,12 @@ include(":app")
 //include(":linkpreview")
 include(":data:proto")
 
-include("cmix")
-project(":cmix").projectDir = File("elixxir-dapps-sdk-kotlin/cmix")
+include(":xxclient:cmix")
+project(":xxclient:cmix").projectDir = File("elixxir-dapps-sdk-kotlin/cmix")
 
 include("xxclient")
 project(":xxclient").projectDir = File("elixxir-dapps-sdk-kotlin/xxclient")
 
 include(":feature:registration")
 include(":data:session")
+include(":core:ui")
