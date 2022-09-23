13f682de82c7f3df96fe62ae3eae1299f509cf33
Kamal Bramwell <kamal@elixxir.io>
2022-09-23 12:39:59 -0400

    Removed unused code

--- app/src/main/java/io/xxlabs/messenger/MessengerApp.kt
+++ app/src/main/java/io/xxlabs/messenger/MessengerApp.kt
@@ -1,12 +1,7 @@
 package io.xxlabs.messenger
 
 import android.app.Application
-import io.xxlabs.messenger.config.ClientBridge
-import io.xxlabs.messenger.config.DefaultClient
 
 class MessengerApp : Application() {
 
-    val clientBridge: ClientBridge by lazy {
-        DefaultClient()
-    }
 }
