d74bbc78eeee07d7a46e88a5a5dfb351e213a4fb
Kamal Bramwell <kamal@elixxir.io>
2022-09-23 10:36:41 -0400

    Cleaned up project-level build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
     repositories {
@@ -6,29 +6,22 @@ buildscript {
         maven(url = "https://maven.google.com")
         maven(url = "https://plugins.gradle.org/m2/")
         maven(url = "https://jitpack.io")
-        maven(url = "https://kotlin.bintray.com/kotlinx/")
-        maven(url = "https://maven.appspector.com/artifactory/android-sdk")
     }
 
     dependencies {
-        classpath("org.jacoco:org.jacoco.core:0.8.7")
-        classpath("com.android.tools.build:gradle:7.1.2")
-        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.18")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-        classpath("com.google.gms:google-services:4.3.10")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.7.1")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.1")
+        classpath("com.google.protobuf:protobuf-gradle-plugin:${Plugins.PROTOBUF}")
+        classpath("com.google.gms:google-services:${Plugins.GOOGLE_SVCS}")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:${Plugins.CRASHLYTICS}")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${Plugins.NAVIGATION_SAFEARGS}")
     }
 }
 
 allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven(url = "https://jitpack.io")
-        maven(url = "https://maven.appspector.com/artifactory/android-sdk")
-        maven(url = "https://oss.jfrog.org/libs-snapshot")
-        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions.freeCompilerArgs += listOf(
+            "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi",
+            "-opt-in=androidx.compose.material.ExperimentalMaterialApi",
+        )
     }
 }
 
