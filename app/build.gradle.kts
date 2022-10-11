2925a7df5a2a2f896785bcf53dfc37d83ffa40e0
Kamal Bramwell <kamal@elixxir.io>
2022-10-11 11:25:22 -0400

    Version up 637/2.95

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 
     defaultConfig {
         applicationId = "io.xxlabs.messenger"
-        versionCode = 635
+        versionCode = 637
         versionName = "2.95"
         minSdk = 26
         targetSdk = 31
