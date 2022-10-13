4b96dc8cd900a75d979fa286346dece755e91a1f
Kamal Bramwell <kamal@elixxir.io>
2022-10-13 13:26:40 -0400

    Implemented MessengerIsLoaded

--- xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerIsLoaded.kt
+++ xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerIsLoaded.kt
@@ -1,4 +1,10 @@
 package io.elixxir.xxmessengerclient.commands
 
-class MessengerIsLoaded {
+import io.elixxir.xxmessengerclient.MessengerEnvironment
+
+class MessengerIsLoaded(private val env: MessengerEnvironment) {
+
+    operator fun invoke(): Boolean {
+        return env.cMix != null
+    }
 }
