d234ba8b0e09e681f2c0358ca5c11120c4bc67de
Kamal Bramwell <kamal@elixxir.io>
2022-10-13 16:02:56 -0400

    Implemented MessengerStart

--- xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerStart.kt
+++ xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerStart.kt
@@ -1,4 +1,16 @@
 package io.elixxir.xxmessengerclient.commands
 
-class MessengerStart {
+import io.elixxir.xxclient.models.NetworkFollowerStatus
+import io.elixxir.xxmessengerclient.MessengerEnvironment
+import io.elixxir.xxmessengerclient.utils.MessengerException
+
+class MessengerStart(private val env: MessengerEnvironment) {
+
+    operator fun invoke(timeoutMs: Long = 30_000) {
+        env.cMix?.let {
+            if (it.getNetworkFollowerStatus() != NetworkFollowerStatus.Running) {
+                it.startNetworkFollower(timeoutMs)
+            }
+        } ?: throw MessengerException.NotLoaded()
+    }
 }
---
+++ xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/utils/Exceptions.kt
@@ -0,0 +1,6 @@
+package io.elixxir.xxmessengerclient.utils
+
+sealed class MessengerException(message: String?) : Exception(message) {
+
+    class NotLoaded : MessengerException(null)
+}
