e2171d1f114a703452adab0e67dc34f6a0cea97d
Kamal Bramwell <kamal@elixxir.io>
2022-10-17 16:57:03 -0400

    Implemented MessengerRegister

--- xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerRegister.kt
+++ xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerRegister.kt
@@ -1,4 +1,25 @@
 package io.elixxir.xxmessengerclient.commands
 
-class MessengerRegister {
+import io.elixxir.xxclient.models.ContactAdapter
+import io.elixxir.xxmessengerclient.MessengerEnvironment
+import io.elixxir.xxmessengerclient.utils.MessengerException
+
+class MessengerRegister(private val env: MessengerEnvironment) {
+
+    private val altUdAddress = "46.101.98.49:18001"
+
+    operator fun invoke(username: String) {
+        val cMix = env.cMix ?: throw MessengerException.NotLoaded("CMix")
+        val e2e = env.e2e ?: throw MessengerException.NotLoaded("E2E")
+
+        env.ud = env.newOrLoadUd(
+            e2eId = e2e.id,
+            networkFollowerStatus = cMix.getNetworkFollowerStatus(),
+            username = username,
+            registrationValidationSignature = cMix.receptionRegistrationValidationSignature,
+            certificateData = env.udCert ?: byteArrayOf(),
+            contact = ContactAdapter(env.udContact ?: byteArrayOf()),
+            udIpAddress = altUdAddress
+        ).getOrThrow()
+    }
 }
--- xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/environment/NewOrLoadUd.kt
+++ xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/environment/NewOrLoadUd.kt
@@ -4,33 +4,31 @@ import io.elixxir.xxclient.bindings.Bindings
 import io.elixxir.xxclient.models.Contact
 import io.elixxir.xxclient.models.NetworkFollowerStatus
 import io.elixxir.xxclient.userdiscovery.UserDiscovery
-import io.elixxir.xxclient.utils.Certificate
 import io.elixxir.xxclient.utils.CertificateData
 import io.elixxir.xxclient.utils.E2eId
 import io.elixxir.xxclient.utils.Signature
 import io.elixxir.xxmessengerclient.utils.nonNullResultOf
 
-class NewOrLoadUd(
-    private val bindings: Bindings,
-    private val e2eId: () -> E2eId,
-    private val networkFollowerStatus: () -> NetworkFollowerStatus,
-    private val username: () -> String,
-    private val registrationValidationSignature: () -> Signature,
-    private val certificateData: () -> CertificateData,
-    private val contact: () -> Contact,
-    private val udIpAddress: () -> String,
-) {
+class NewOrLoadUd(private val bindings: Bindings) {
 
-    operator fun invoke(): Result<UserDiscovery> {
+    operator fun invoke(
+        e2eId: E2eId,
+        networkFollowerStatus: NetworkFollowerStatus,
+        username: String,
+        registrationValidationSignature: Signature,
+        certificateData: CertificateData,
+        contact: Contact,
+        udIpAddress: String,
+    ): Result<UserDiscovery> {
         return nonNullResultOf {
             bindings.getOrCreateUd(
-                e2eId(),
-                networkFollowerStatus(),
-                username(),
-                registrationValidationSignature(),
-                certificateData(),
-                contact(),
-                udIpAddress()
+                e2eId,
+                networkFollowerStatus,
+                username,
+                registrationValidationSignature,
+                certificateData,
+                contact,
+                udIpAddress
             )
         }
     }
