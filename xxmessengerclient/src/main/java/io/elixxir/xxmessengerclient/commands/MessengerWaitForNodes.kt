f5795e9081002a9db8721bb794b6b17319eba73b
Kamal Bramwell <kamal@elixxir.io>
2022-10-17 17:57:20 -0400

    Implemented MessengerWaitForNodes

--- xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerWaitForNodes.kt
+++ xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerWaitForNodes.kt
@@ -1,4 +1,25 @@
 package io.elixxir.xxmessengerclient.commands
 
-class MessengerWaitForNodes {
+import io.elixxir.xxmessengerclient.MessengerEnvironment
+import io.elixxir.xxmessengerclient.utils.MessengerException
+
+class MessengerWaitForNodes(private val env: MessengerEnvironment) {
+
+    operator fun invoke(
+        retries: Int = 10,
+        retryIntervalMs: Long,
+        targetRatio: Float = 0.75F,
+    ) {
+        val cMix = env.cMix ?: throw MessengerException.NotLoaded("CMix")
+        var report = cMix.getNodeRegistrationStatus()
+        var _retries = retries
+
+        while (report.ratio < targetRatio && _retries > 0) {
+            env.sleep(retryIntervalMs)
+            report = cMix.getNodeRegistrationStatus()
+            _retries--
+        }
+
+        if (report.ratio < targetRatio) throw MessengerException.TimedOut()
+    }
 }
