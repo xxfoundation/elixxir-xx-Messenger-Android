3aeee5d1042d08ae4fd6b21cedf87acd956f67f4
Kamal Bramwell <kamal@elixxir.io>
2022-10-17 16:42:10 -0400

    Implemented MessengerListenForMessages

--- xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerListenForMessages.kt
+++ xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerListenForMessages.kt
@@ -1,4 +1,23 @@
 package io.elixxir.xxmessengerclient.commands
 
-class MessengerListenForMessages {
+import io.elixxir.xxclient.models.MessageType
+import io.elixxir.xxmessengerclient.MessengerEnvironment
+import io.elixxir.xxmessengerclient.utils.MessengerException
+
+class MessengerListenForMessages(private val env: MessengerEnvironment) {
+
+    operator fun invoke() {
+        val e2e = env.e2e ?: throw MessengerException.NotLoaded("E2E")
+        try {
+            e2e.registerListener(
+                senderId = byteArrayOf(),
+                messageType = MessageType.XxMessage,
+                e2eListener = env.messageListeners.listeners.values.first()
+            )
+            env.isListeningForMessages = { true }
+        } catch (e: Exception) {
+            env.isListeningForMessages = { false }
+            throw e
+        }
+    }
 }
