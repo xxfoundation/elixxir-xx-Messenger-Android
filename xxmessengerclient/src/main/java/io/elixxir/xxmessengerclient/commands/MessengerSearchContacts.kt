84ad1e6fafa9514183355f7a6494d53efe709e88
Kamal Bramwell <kamal@elixxir.io>
2022-10-18 09:52:34 -0400

    Implemented MessengerSearchContacts

--- xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerSearchContacts.kt
+++ xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerSearchContacts.kt
@@ -1,4 +1,26 @@
 package io.elixxir.xxmessengerclient.commands
 
-class MessengerSearchContacts {
+import io.elixxir.xxclient.callbacks.UdSearchResultListener
+import io.elixxir.xxclient.models.Fact
+import io.elixxir.xxmessengerclient.MessengerEnvironment
+import io.elixxir.xxmessengerclient.utils.MessengerException
+
+
+class MessengerSearchContacts(private val env: MessengerEnvironment) {
+
+    operator fun invoke(
+        query: List<Fact>,
+        listener: UdSearchResultListener
+    ) {
+        val e2e = env.e2e ?: throw MessengerException.NotLoaded("E2E")
+        val ud = env.ud ?: throw MessengerException.NotLoaded("UD")
+
+        env.searchUD(
+            e2eId = e2e.id,
+            udContact = ud.contact,
+            facts = query,
+            searchResultListener = listener,
+            singleUseParams = env.getSingleUseParams().getOrThrow()
+        )
+    }
 }
--- xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/environment/SearchUD.kt
+++ xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/environment/SearchUD.kt
@@ -2,31 +2,30 @@ package io.elixxir.xxmessengerclient.environment
 
 import io.elixxir.xxclient.bindings.Bindings
 import io.elixxir.xxclient.callbacks.UdSearchResultListener
-import io.elixxir.xxclient.models.BindingsModel.Companion.encode
+import io.elixxir.xxclient.models.BindingsModel.Companion.encodeArray
 import io.elixxir.xxclient.models.Contact
-import io.elixxir.xxclient.models.FactsList
+import io.elixxir.xxclient.models.Fact
 import io.elixxir.xxclient.models.SingleUseReport
 import io.elixxir.xxclient.utils.E2eId
 import io.elixxir.xxclient.utils.SingleUseParams
 import io.elixxir.xxmessengerclient.utils.nonNullResultOf
 
-class SearchUD(
-    private val bindings: Bindings,
-    private val e2eId: () -> E2eId,
-    private val udContact: () -> Contact,
-    private val searchResultListener: () -> UdSearchResultListener,
-    private val facts: () -> FactsList,
-    private val singleUseParams: () -> SingleUseParams
-) {
+class SearchUD(private val bindings: Bindings) {
 
-    operator fun invoke(): Result<SingleUseReport> {
+    operator fun invoke(
+        e2eId: E2eId,
+        udContact: Contact,
+        facts: List<Fact>,
+        singleUseParams: SingleUseParams,
+        searchResultListener: UdSearchResultListener,
+    ): Result<SingleUseReport> {
         return nonNullResultOf {
             bindings.searchUd(
-                e2eId(),
-                udContact(),
-                searchResultListener(),
-                encode(facts()),
-                singleUseParams()
+                e2eId,
+                udContact,
+                searchResultListener,
+                encodeArray(facts),
+                singleUseParams
             )
         }
     }
