4897986fb682c12f585e4f82fc802a5f492cf79e
Kamal Bramwell <kamal@elixxir.io>
2022-10-18 09:34:19 -0400

    Implemented MessengerDestroy

--- xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerDestroy.kt
+++ xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerDestroy.kt
@@ -1,4 +1,28 @@
 package io.elixxir.xxmessengerclient.commands
 
-class MessengerDestroy {
+import io.elixxir.xxclient.models.NetworkFollowerStatus
+import io.elixxir.xxmessengerclient.MessengerEnvironment
+import io.elixxir.xxmessengerclient.utils.MessengerException
+
+class MessengerDestroy(private val env: MessengerEnvironment) {
+
+    operator fun invoke(
+        runningProcessPollingIntervalMs: Long = 1000
+    ) {
+        val cmix = env.cMix ?: throw(MessengerException.NotLoaded("CMix"))
+        if (cmix.getNetworkFollowerStatus() == NetworkFollowerStatus.Running) {
+            cmix.stopNetworkFollower()
+        }
+        while (cmix.hasRunningProcesses()) {
+            env.sleep(runningProcessPollingIntervalMs)
+        }
+        env.apply {
+            ud = null
+            e2e = null
+            cMix = null
+            isListeningForMessages = false
+            fileManager.removeItem(env.storageDir)
+            passwordStorage.clear()
+        }
+    }
 }
