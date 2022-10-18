b627eea6f78fbf1e5631e7a0991fd28371718ac7
Kamal Bramwell <kamal@elixxir.io>
2022-10-18 10:30:07 -0400

    Created TODOs for SendMessage and VerifyContact
    The legacy implementation will temporarily call the appropriate E2E methods directly

--- xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerSendMessage.kt
+++ xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerSendMessage.kt
@@ -1,4 +1,26 @@
 package io.elixxir.xxmessengerclient.commands
 
-class MessengerSendMessage {
+import io.elixxir.xxclient.models.MessageType
+import io.elixxir.xxclient.utils.Payload
+import io.elixxir.xxclient.utils.UserId
+import io.elixxir.xxmessengerclient.MessengerEnvironment
+import io.elixxir.xxmessengerclient.utils.MessengerException
+
+class MessengerSendMessage(private val env: MessengerEnvironment) {
+
+    operator fun invoke(
+        receiver: UserId,
+        payload: Payload,
+    ) {
+        TODO("Use legacy implementation to call the E2E method directly")
+//        val e2e = env.e2e ?: throw MessengerException.NotLoaded("E2E")
+//        val ud = env.ud ?: throw MessengerException.NotLoaded("UD")
+//
+//        val report = e2e.sendE2e(
+//            messageType = MessageType.XxMessage,
+//            receiverId = receiver,
+//            payload = payload,
+//            params = env.getE2EParams()
+//        )
+    }
 }
--- xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerVerifyContact.kt
+++ xxmessengerclient/src/main/java/io/elixxir/xxmessengerclient/commands/MessengerVerifyContact.kt
@@ -1,4 +1,35 @@
 package io.elixxir.xxmessengerclient.commands
 
-class MessengerVerifyContact {
+import io.elixxir.xxclient.callbacks.UdLookupResultListener
+import io.elixxir.xxclient.models.Contact
+import io.elixxir.xxmessengerclient.MessengerEnvironment
+
+class MessengerVerifyContact(private val env: MessengerEnvironment) {
+
+    operator fun invoke(
+        contact: Contact,
+        listener: UdLookupResultListener,
+    ) {
+        TODO("Use legacy implementation to call the methods directly")
+//        val e2e = env.e2e ?: throw MessengerException.NotLoaded("E2E")
+//        val ud = env.ud ?: throw MessengerException.NotLoaded("UD")
+//        val facts = contact.getFactsFromContact()
+//        var verifiedContact: Contact?
+//
+//        if (facts.isEmpty()) {
+//            env.lookupUD(
+//                e2eId = e2e.id,
+//                contact = ud.contact,
+//                listener = listener,
+//                lookupId = contact.getIdFromContact(),
+//                singleUseParams = env.getSingleUseParams()
+//            )
+//        }
+//
+//        e2e.verifyOwnership(
+//            receivedContact = contact,
+//            verifiedContact = TODO(),
+//            e2eId = e2e.id
+//        )
+    }
 }
