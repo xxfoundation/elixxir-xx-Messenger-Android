5274994aca7b7459d16981c8a0142266fc7434c4
Kamal Bramwell <kamal@elixxir.io>
2022-11-16 08:38:06 -0500

    Added factory method

--- app/src/main/java/io/xxlabs/messenger/data/datatype/FactType.kt
+++ app/src/main/java/io/xxlabs/messenger/data/datatype/FactType.kt
@@ -4,5 +4,9 @@ enum class FactType(val value: Long) {
     USERNAME(0),
     EMAIL(1),
     PHONE(2),
-    NICKNAME(3)
+    NICKNAME(3);
+
+    companion object  {
+        fun from(value: Long): FactType? = values().firstOrNull { it.value == value }
+    }
 }
